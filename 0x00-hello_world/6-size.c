e86121ba7119c271876b4d44b45b41909157af6c
vinmmuriuki <vinmmuriuki@gmail.com>
2023-02-21 03:01:10 -0800

    task6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("Size of double: %zu bytes\n", sizeof(doubleType));
+	printf("Size of float: %zu bytes\n", sizeof(floatType));
+	return (0);
+}
